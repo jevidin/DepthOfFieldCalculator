f42d743dd08a49c2b995dd3005f94563777a9bd7
Sean <sean_tam@sfu.ca>
2020-10-05 17:39:47 -0700

    initial commit

---
+++ app/src/main/java/com/example/assignment2/model/LensManager.java
@@ -0,0 +1,37 @@
+package com.example.assignment2.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Data model: Store a collection of lenses.
+ */
+public class LensManager implements Iterable<Lens>{
+    private static LensManager instance;
+    private List<Lens> lenses = new ArrayList<>();
+    public static LensManager getInstance(){
+        if (instance == null){
+            instance = new LensManager();
+        }
+        return instance;
+    }
+    public void add(Lens lens) {
+        lenses.add(lens);
+    }
+    public void remove(Lens lens) {
+        lenses.remove(lens);
+    }
+    public Lens get(int i) {
+        return lenses.get(i);
+    }
+
+    @Override
+    public Iterator<Lens> iterator() {
+        return lenses.iterator();
+    }
+
+    public int getNumLenses() {
+        return lenses.size();
+    }
+}
