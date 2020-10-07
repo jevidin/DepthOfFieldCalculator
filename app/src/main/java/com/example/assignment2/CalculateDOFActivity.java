7bf1e1d367ed3680d6b621a227578ccdee016ff9
jzl <jzl5@sfu.ca>
2020-10-06 18:59:41 -0700

    [jzl5] Add buttons and text, as well as edittext
    DOF Calculating not yet implemented

--- app/src/main/java/com/example/assignment2/CalculateDOFActivity.java
+++ app/src/main/java/com/example/assignment2/CalculateDOFActivity.java
@@ -6,17 +6,68 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.assignment2.model.Lens;
+import com.example.assignment2.model.LensManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CalculateDOFActivity extends AppCompatActivity {
+    private static final String EXTRA_LENS_INDEX = "com.example.assignment2.CalculateDOFActivity - selected lens";
+    private LensManager lensManager;
+    private Lens selectedLens;
+
+    private String[] viewCalculated = {"Near Focal Point: Enter all values", "Far Focal Point: Enter all values",
+                                    "Depth of Field: Enter all values", "Hyper Focal Point: Enter all values"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_dof);
 
+        lensManager = LensManager.getInstance();
+        extractDataFromIntent();
+        displayLens();
+        setUpCalculateBtn();
         setUpBackBtn();
     }
+
+    private void setUpCalculateBtn() {
+        Button btn = findViewById(R.id.calculateBtn);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText textInputCOC = findViewById(R.id.inputCOC);
+                double COC = Double.parseDouble(textInputCOC.getText().toString());
+                EditText textInputDistance = findViewById(R.id.inputDistance);
+                double distance = Double.parseDouble(textInputDistance.getText().toString());
+                EditText textInputAperture = findViewById(R.id.inputAperture);
+                double aperture = Double.parseDouble(textInputAperture.getText().toString());
+
+            }
+        });
+    }
+
+    private void displayLens() {
+        String lensDetail = getString(R.string.displayLensDetails);
+        lensDetail += " " + selectedLens.getDescription();
+        TextView viewLens = (TextView)findViewById(R.id.viewLens);
+        viewLens.setText(lensDetail);
+    }
+
+    private void extractDataFromIntent() {
+        Intent intent = getIntent();
+        int lensIdx = intent.getIntExtra(EXTRA_LENS_INDEX, 0);
+        selectedLens = lensManager.get(lensIdx);
+    }
+
 
     private void setUpBackBtn() {
         Button btn = findViewById(R.id.Calculate_btnBack);
@@ -28,9 +79,9 @@ public class CalculateDOFActivity extends AppCompatActivity {
         });
     }
 
-    public static Intent makeLaunchIntent(Context context){
+    public static Intent makeLaunchIntent(Context context, int lensIdx){
         Intent intent = new Intent(context, CalculateDOFActivity.class);
-        //intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_LENS_INDEX, lensIdx);
         return intent;
     }
 }
